043f2f650e1424438776f3a0dd4268de4c38aeba
Diana <miauperuvian@gmail.com>
2026-04-18 16:38:55 -0500

    feat(app): add developer registration handling and reset functionality.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,24 @@
 import { Component, signal } from '@angular/core';
+import {DeveloperRegistration} from './greetings/presentation/components/developer-registration/developer-registration';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [DeveloperRegistration],
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('hello-angular-developer');
+  public firstName: string ='';
+  public lastName: string ='';
+
+  public updateRegisteredDeveloperInfo(developer:{firstName:string, lastName:string}): void{
+    this.firstName=developer.firstName;
+    this.lastName=developer.lastName;
+}
+
+public resetRegisteredDeveloperInfo(): void {
+    this.firstName='';
+    this.lastName='';
+}
 }
